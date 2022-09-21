2b3871e4a45bb90ed9f4e6eda2103cd6958c994e
FeniasCossa <fenisasebastiao636@gmail.com>
2022-09-21 13:10:48 +0200

    footer creation,update and logout of contact and javaScript event creation

--- script.js
+++ script.js
@@ -15,6 +15,20 @@ $(document).ready(function(){
         $('html').animate({ scrollTop: 0 });
     });
 
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Desenvolvedor", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-1", {
+        strings: ["YouTuber", "Desenvolvedor", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
